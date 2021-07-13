e627f348c5fe0cf5a5d4801775667ed822072255
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-07-13 17:50:51 -0300

    Configuração do projeto e mapeamento da primeira rota

---
+++ src/main/java/br/com/jun/regescweb/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.jun.regescweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
