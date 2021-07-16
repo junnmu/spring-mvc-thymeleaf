58120e13d5fd2237e32b6d43e1c725c94f5aa15a
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-07-15 21:05:00 -0300

    Detalhes do professor

--- src/main/java/br/com/jun/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/jun/regescweb/controllers/ProfessorController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -46,6 +48,21 @@ public class ProfessorController {
         else {
             Professor professor = requisicao.toProfessor();
             professorRepository.save(professor);
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id) {
+        Optional<Professor> optional = professorRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+        }
+        else {
             return new ModelAndView("redirect:/professores");
         }
     }
